b575f1fc9bf5abb1eb8f1fa46b94a8c3ae602a41
Alexander Stoffel <alex@stoffel.io>
2021-10-01 19:33:51 +0700

    move logic to class

--- src/Widgets/ContinueEditing.php
+++ src/Widgets/ContinueEditing.php
@@ -3,6 +3,7 @@
 namespace Webographen\ContinueEditing\Widgets;
 
 use Statamic\Widgets\Widget;
+use Statamic\Facades\Entry;
 use Statamic\Facades\Collection as CollectionAPI;
 
 class ContinueEditing extends Widget
@@ -15,24 +16,31 @@ class ContinueEditing extends Widget
     public function html()
     {
         $collections = $this->config('collections', '*');
-        $error = "";
+        $limit =  $this->config('limit', 5);
 
-        // make sure all collections exist
-        if ($collections != "*") {
+        if (strpos("*", $collections) !== false) {
+            $results = Entry::query()
+                            ->orderBy('updated_at', 'desc')
+                            ->limit($limit)
+                            ->get();
+        } else {
             foreach (explode("|", $collections) as $collection) {
                 if (!CollectionAPI::handleExists($collection)) {
-                    $error .= "Error: Collection [$collection] doesn't exist.<br />";
+                    return "Error: Collection [$collection] doesn't exist.";
                 }
             }
+            $results = Entry::query()
+                            ->whereIn('collection', explode("|", $collections))
+                            ->orderBy('updated_at', 'desc')
+                            ->limit($limit)
+                            ->get();
         }
 
         $icon = \Statamic::svg('content-writing');
 
         return view('webographen::widgets.continue_editing', [
-            'error' => $error,
-            'collections' => $collections,
-            'icon' => $icon,
-            'limit' => $this->config('limit', 5),
+            'results' => $results,
+            'icon' => $icon
         ]);
     }
 }
