fbe956447101ab7eb58016106d16e0cb9d2d1348
Quinten Buis <36452184+quintenbuis@users.noreply.github.com>
2023-06-06 15:02:33 +0200

    Fixes colors

--- resources/views/widgets/continue_editing.blade.php
+++ resources/views/widgets/continue_editing.blade.php
@@ -1,7 +1,7 @@
 <div class="h-full p-0 card">
     <header class="flex items-center justify-between p-2 border-b">
         <h2 class="flex items-center">
-            <div class="w-6 h-6 mr-1 text-grey-80">
+            <div class="w-6 h-6 mr-1 text-gray-800">
                 @cp_svg('icons/light/content-writing')
             </div>
             <span>{{ __('Continue Editing') }}</span>
@@ -18,7 +18,7 @@
                         {{ $result->collection()->id() }}
                     </span> 
                 </div>
-                <div class="pl-2 text-grey-70">
+                <div class="pl-2 text-gray-700">
                     {{ $result->updated_at->format(config('statamic.cp.date_format')) }}
                 </div>
             </div>
